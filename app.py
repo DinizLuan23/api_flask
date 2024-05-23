eedf99be07b44df154b775d45e71463033ca45a9
Luan Diniz <luan.diniz23@hotmail.com>
2024-05-23 18:00:25 -0300

    testes com pytest

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def create_task():
    task_id_control += 1
    tasks.append(new_task)
 
-   return jsonify({"messagem": "Nova tarefa criada com sucesso!"})
+   return jsonify({ "message": "Nova tarefa criada com sucesso!", "id": new_task.id })
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
@@ -57,6 +57,7 @@ def delete_task(id):
    for t in tasks:
       if t.id == id:
          task = t
+         break
 
    if task is None:
       return jsonify({ 'message': 'Não foi possível encontrar a atividade' }), 404
